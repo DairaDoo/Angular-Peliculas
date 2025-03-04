355252e9c2a402a8ede922f46aa306c7fe4fd656
DairanDoo <your_email@example.com>
2025-03-04 10:24:07 -0400

    *NgFor vs For (antigua forma de iterar sobre elementos de una colleción vs la nueva forma de hacerlo).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgFor, CurrencyPipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
-import { CurrencyPipe } from '@angular/common';
 
 // Los Pipes convierten data de un formato a otro
 // En este caso se utilizan con la fecha usando DatePipe
 
 @Component({
   selector: 'app-root',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe,  NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,11 +14,18 @@ export class AppComponent {
   title = 'angular-peliculas';
   apellido = 'De Jesús';
   edad = 100;
-  pelicula = {
-    titulo: "Spider-Man",
-    fecha_lanzamiento: new Date(),
-    precio: 1400.99,
-  }
+  peliculas = [
+    {
+      titulo: "Spider-Man",
+      fecha_lanzamiento: new Date(),
+      precio: 1400.99,
+    },
+    {
+      titulo: "Moana",
+      fecha_lanzamiento: new Date("2016-05-03"),
+      precio: 300.09,
+    }
+  ]
 
   saludar(nombre: string): string {
     return `Saludos ${nombre}`
