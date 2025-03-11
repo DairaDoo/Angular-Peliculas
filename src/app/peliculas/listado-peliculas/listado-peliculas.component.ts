9ef8d30bd29b6091daf80ea5c9606fa6b545e540
DairanDoo <your_email@example.com>
2025-03-11 14:41:13 -0400

    Usando @Input para recibir parámetros en los componentes e interpolación para mostrar los elementos en un archivo .ts

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
@@ -10,44 +9,10 @@ import { DatePipe, CurrencyPipe } from '@angular/common';
 })
 export class ListadoPeliculasComponent implements OnInit {
   ngOnInit(): void {
-    setTimeout(() => {
-      this.peliculas = [{
-        titulo: 'Inside Out 2',
-        fecha_lanzamiento: new Date(),
-        precio: 1400.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
-      },
-      {
-        titulo: 'Moana 2',
-        fecha_lanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
-      },
-      {
-        titulo: 'Bad Boys: Ride or Die',
-        fecha_lanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
-      },
-      {
-        titulo: 'Deadpool & Wolverine',
-        fecha_lanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
-      },
-      {
-        titulo: 'Oppenheimer',
-        fecha_lanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
-      },
-      {
-        titulo: 'The Flash',
-        fecha_lanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
-      }];
-    }, 2000)
+
   }
-  peliculas! : any[];
+
+  // añadimos el input para recibir este parametro siempre al llamar el componente
+  @Input({required: true})
+    peliculas! : any[];
 }
