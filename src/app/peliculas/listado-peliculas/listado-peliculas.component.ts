75539263d883e01b26d408cdea740ae526e83bf6
DairanDoo <your_email@example.com>
2025-03-22 13:26:47 -0400

    Eliminando importaciones innecesarias que no se estan utilizando.

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -5,7 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, CurrencyPipe, ListadoGenericoComponent, MatButtonModule, MatIconModule],
+  imports: [ListadoGenericoComponent, MatButtonModule, MatIconModule],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
