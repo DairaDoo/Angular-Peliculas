4616bba01a0acebb34da94487b62c11fad9ed682
DairanDoo <your_email@example.com>
2025-03-28 16:38:43 -0400

    Manejo de formularios reactivos utilizando FormBuilder y también matformfield de angular material para crear las interfaces, además se agrego retroalimentación utilizando Validators, en este caso el required y un mensaje de error personalizado también para retroalimentación dentro de una función que se llama el mat-error usando intepolación.

--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,18 +1,43 @@
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-crear-generos',
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
   templateUrl: './crear-generos.component.html',
-  styleUrl: './crear-generos.component.css'
+  styleUrl: './crear-generos.component.css',
 })
 export class CrearGenerosComponent {
-  router = inject(Router);
+  private router = inject(Router);
+  private formbuilder = inject(FormBuilder);
+
+  form = this.formbuilder.group({
+    nombre: ['', { validators: [Validators.required] }],
+  });
+
+  obtenerErrorCampoNombre(): string {
+    let nombre = this.form.controls.nombre;
+
+    if (nombre.hasError('required')) {
+      return "El campo nombre es requerido."
+    }
+
+    return "";
+  }
 
   guardarCambios() {
     // ... simulamos guardar los cambios
-    this.router.navigate(["/generos"])
+    // this.router.navigate(['/generos']);
+    console.log(this.form.value);
   }
 }
