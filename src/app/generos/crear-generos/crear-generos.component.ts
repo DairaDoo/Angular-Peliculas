84290b78bfe34c24ad42d94249a9f739620e23f8
DairanDoo <your_email@example.com>
2025-04-01 15:48:29 -0400

    Utilizando validaciones personalizadas y dividiendo lógica del form afuera de crear-generos para que sea reutilizable, se utlizo input y output también para pasar argumentos bidireccional.

--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -4,40 +4,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { primeraLetraMayuscula } from '../../compartidos/funciones/validaciones';
+import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
+import { GeneroCreacionDTO } from '../generos';
 
 @Component({
   selector: 'app-crear-generos',
-  imports: [
-    MatButtonModule,
-    RouterLink,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-  ],
+  imports: [FormularioGeneroComponent],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css',
 })
 export class CrearGenerosComponent {
   private router = inject(Router);
-  private formbuilder = inject(FormBuilder);
 
-  form = this.formbuilder.group({
-    nombre: ['', { validators: [Validators.required] }],
-  });
-
-  obtenerErrorCampoNombre(): string {
-    let nombre = this.form.controls.nombre;
-
-    if (nombre.hasError('required')) {
-      return "El campo nombre es requerido."
-    }
-
-    return "";
-  }
-
-  guardarCambios() {
+  guardarCambios(genero: GeneroCreacionDTO) {
     // ... simulamos guardar los cambios
     // this.router.navigate(['/generos']);
-    console.log(this.form.value);
+    console.log('Creando el genero: ', genero);
+
   }
 }
