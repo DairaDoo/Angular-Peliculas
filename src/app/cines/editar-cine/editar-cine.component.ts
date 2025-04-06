7189c869375040c899ff171b301327bc77b28afb
DairanDoo <your_email@example.com>
2025-04-06 14:37:25 -0400

    Creando formulario en la sección de cines

--- src/app/cines/editar-cine/editar-cine.component.ts
+++ src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CineCreacionDTO, CineDTO } from '../cines';
+import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './editar-cine.component.html',
   styleUrl: './editar-cine.component.css'
 })
 export class EditarCineComponent {
   @Input({transform: numberAttribute})
-    id!: number;
+  id!: number;
+
+  cine: CineDTO = {id: 1, nombre: 'Caribean Cinemas'}
+
+
+  guardarCambios(cine: CineCreacionDTO) {
+    console.log("Editando cine: ", cine);
+  }
+
 }
