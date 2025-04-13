244305ee254fe9a3b75e3727f22613771f43286c
DairanDoo <your_email@example.com>
2025-04-12 21:35:50 -0400

    Creando formulario para crear y editar películas.

--- src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
+++ src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { PeliculaCreacionDTO } from '../peliculas';
+import { FormularioActoresComponent } from "../../actores/formulario-actores/formulario-actores.component";
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
 
 @Component({
   selector: 'app-crear-pelicula',
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './crear-pelicula.component.html',
   styleUrl: './crear-pelicula.component.css'
 })
 export class CrearPeliculaComponent {
+
+  guardarCambios(pelicula: PeliculaCreacionDTO) {
+    console.log('Creando película', pelicula)
+  }
 
 }
