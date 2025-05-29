520156690f8d3c824a0d3e9ee7f9dd2f6324294e
DairanDoo <your_email@example.com>
2025-05-28 22:19:56 -0400

    Mostrando errores del Web API en nuestro frontend con Angular, implementando validación con crear Genero de máximo de caracteres

--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -8,10 +8,12 @@ import { primeraLetraMayuscula } from '../../compartidos/funciones/validaciones'
 import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
 import { GeneroCreacionDTO } from '../generos';
 import { GenerosService } from '../generos.service';
+import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
+import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
 
 @Component({
   selector: 'app-crear-generos',
-  imports: [FormularioGeneroComponent],
+  imports: [FormularioGeneroComponent, MostrarErroresComponent],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css',
 })
@@ -19,14 +21,23 @@ export class CrearGenerosComponent {
 
   private router = inject(Router);
   private generosService = inject(GenerosService);
+  errores: string[] = [];
 
   guardarCambios(genero: GeneroCreacionDTO) {
 
     // ... guardar los cambios
 
-    this.generosService.crear(genero).subscribe(() => {
+    this.generosService.crear(genero).subscribe({
+      next: () => {
       this.router.navigate(['/generos']);
+    },
+      error: err => {
+        const errores = extraerErrores(err);
+        this.errores = errores;
+      }
     });
+
+
 
   }
 }
--- src/app/generos/formulario-genero/formulario-genero.component.ts
+++ src/app/generos/formulario-genero/formulario-genero.component.ts
@@ -37,7 +37,7 @@ export class FormularioGeneroComponent implements OnInit {
   private formbuilder = inject(FormBuilder);
 
   form = this.formbuilder.group({
-    nombre: ['', {validators: [Validators.required, primeraLetraMayuscula()]}],
+    nombre: ['', {validators: [Validators.required, primeraLetraMayuscula(), Validators.maxLength(50)]}],
   })
 
   obtenerErrorCampoNombre(): string {
@@ -45,6 +45,10 @@ export class FormularioGeneroComponent implements OnInit {
 
     if (nombre.hasError('required')) {
       return 'El campo nombre es requerido.';
+    }
+
+    if (nombre.hasError('maxlength')) {
+      return `El campo nombre no puede tener más de ${nombre.getError('maxlength').requiredLength} caracteres.`;
     }
 
     if (nombre.hasError('primeraLetraMayuscula')) {
