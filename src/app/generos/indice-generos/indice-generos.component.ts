1b0c89329e1d7e95915636df8b760af444bb86c1
DairanDoo <your_email@example.com>
2025-05-31 16:42:05 -0400

    Implementando indice generos para mostrar los generos que devuelve el backend en una tabla utilizando Angular Material y GeneroDTO

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -3,19 +3,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { GenerosService } from '../generos.service';
 import { environment } from '../../../environments/environment';
+import { GeneroDTO } from '../generos';
+import { MatTableModule } from '@angular/material/table';
+import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
 
 @Component({
   selector: 'app-indice-generos',
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, MatTableModule, ListadoGenericoComponent],
   templateUrl: './indice-generos.component.html',
   styleUrl: './indice-generos.component.css'
 })
 export class IndiceGenerosComponent {
   generosService = inject(GenerosService);
+  generos!: GeneroDTO[];
+  columnasAMostrar = ["id", "nombre", "acciones"];
 
   constructor() {
     this.generosService.obtenerTodos().subscribe(generos => {
-      console.log(generos);
+      this.generos = generos;
     })
   }
 }
