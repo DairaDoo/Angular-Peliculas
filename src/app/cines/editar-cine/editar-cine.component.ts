3be5d16a2c9362d44c9b2107684d00e908006570
Dairan De Jesús <dairandemora.work@gmail.com>
2025-06-23 12:59:22 -0400

    Implementando CRUD de Cines en Angular.

--- src/app/cines/editar-cine/editar-cine.component.ts
+++ src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,22 +1,23 @@
 import { Component, Input, numberAttribute } from '@angular/core';
 import { CineCreacionDTO, CineDTO } from '../cines';
 import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
+import { SERVICIO_CRUD_TOKEN } from '../../compartidos/proveedores/proveedores';
+import { CinesService } from '../cines.service';
+import { EditarEntidadComponent } from "../../compartidos/componentes/editar-entidad/editar-entidad.component";
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [FormularioCinesComponent],
+  imports: [EditarEntidadComponent],
   templateUrl: './editar-cine.component.html',
-  styleUrl: './editar-cine.component.css'
+  styleUrl: './editar-cine.component.css',
+  providers: [
+    {provide: SERVICIO_CRUD_TOKEN, useClass: CinesService}
+  ]
 })
 export class EditarCineComponent {
   @Input({transform: numberAttribute})
   id!: number;
 
-  cine: CineDTO = {id: 1, nombre: 'Caribean Cinemas', latitud: 18.493055607172877, longitud: -66.64095215636392};
-
-
-  guardarCambios(cine: CineCreacionDTO) {
-    console.log("Editando cine: ", cine);
-  }
+  formularioCines = FormularioCinesComponent;
 
 }
