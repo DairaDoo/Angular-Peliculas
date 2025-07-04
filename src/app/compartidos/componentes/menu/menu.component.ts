df30538f323dfcfe7e44ba96743648fb40f4608e
Dairan De Jesús <dairandemora.work@gmail.com>
2025-07-04 12:16:29 -0400

    Ocultado elementos del UI dependiendo del ROL del usuario y si este esta autorizado

--- src/app/compartidos/componentes/menu/menu.component.ts
+++ src/app/compartidos/componentes/menu/menu.component.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { AutorizadoComponent } from "../../../seguridad/autorizado/autorizado.component";
 
 @Component({
   selector: 'app-menu',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, AutorizadoComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
