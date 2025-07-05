19dd015ef50233ea59f9ea533b56982dd9027f41
Dairan De Jesús <dairandemora.work@gmail.com>
2025-07-05 09:47:10 -0400

    Implementando Route Guard para proteger acceso no autorizado en distintas rutas de la aplicación

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ListadoPeliculasComponent } from '../peliculas/listado-peliculas/listado-peliculas.component';
 import { PeliculasService } from '../peliculas/peliculas.service';
-import { AutorizadoComponent } from "../seguridad/autorizado/autorizado.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [ListadoPeliculasComponent, AutorizadoComponent],
+  imports: [ListadoPeliculasComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
