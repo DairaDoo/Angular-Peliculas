8e58a0cbe4172fe3f700e55fac0bdb9ca04575d8
DairanDoo <your_email@example.com>
2025-07-20 18:05:48 -0400

    Compartiendo datos entre pruebas

--- src/app/compartidos/componentes/indice-entidad/indice-entidad.component.spec.ts
+++ src/app/compartidos/componentes/indice-entidad/indice-entidad.component.spec.ts
@@ -4,18 +4,27 @@ import { IServicioCRUD } from '../../interfaces/IServicioCRUD';
 import { of } from 'rxjs';
 import { SERVICIO_CRUD_TOKEN } from '../../proveedores/proveedores';
 import { RouterModule } from '@angular/router';
+import { HttpResponse } from '@angular/common/http';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 describe('IndiceEntidadComponent', () => {
+  let mockIServicioCRUD: jasmine.SpyObj<IServicioCRUD<object, object>>;
+
   beforeEach(async () => {
-    const mockIServicioCRUD = jasmine.createSpyObj<
-      IServicioCRUD<object, object>
-    >('IServicioCRUD', ['obtenerPaginado', 'borrar']);
+    mockIServicioCRUD = jasmine.createSpyObj<IServicioCRUD<object, object>>(
+      'IServicioCRUD',
+      ['obtenerPaginado', 'borrar']
+    );
 
     mockIServicioCRUD.obtenerPaginado.and.returnValue(of());
     mockIServicioCRUD.borrar.and.returnValue(of({})); // aca pasamos un objeto vacío como respuesta
 
     await TestBed.configureTestingModule({
-      imports: [IndiceEntidadComponent, RouterModule.forRoot([])],
+      imports: [
+        IndiceEntidadComponent,
+        RouterModule.forRoot([]),
+        SweetAlert2Module.forRoot(),
+      ],
       providers: [
         { provide: SERVICIO_CRUD_TOKEN, useValue: mockIServicioCRUD },
       ],
@@ -40,4 +49,33 @@ describe('IndiceEntidadComponent', () => {
     expect(componente.paginacion.pagina).toBe(1);
   });
 
+  it('Debe de mostrar una tabla cuando existan registros cuando cargarRegistros sea llamada', () => {
+    const fixture = TestBed.createComponent(IndiceEntidadComponent);
+    const componente = fixture.componentInstance;
+    const respuesta = new HttpResponse<object[]>({ body: [{}] });
+
+    mockIServicioCRUD.obtenerPaginado.and.returnValue(of(respuesta));
+
+    componente.cargarRegistros();
+
+    fixture.detectChanges();
+
+    const compilado = fixture.nativeElement as HTMLElement;
+    expect(compilado.querySelectorAll('table').length).toBe(1);
+  });
+
+  it('Debe de NO mostrar una tabla cuando NO existan registros cuando cargarRegistros sea llamada', () => {
+    const fixture = TestBed.createComponent(IndiceEntidadComponent);
+    const componente = fixture.componentInstance;
+    const respuesta = new HttpResponse<object[]>({ body: [] });
+
+    mockIServicioCRUD.obtenerPaginado.and.returnValue(of(respuesta));
+
+    componente.cargarRegistros();
+
+    fixture.detectChanges();
+
+    const compilado = fixture.nativeElement as HTMLElement;
+    expect(compilado.querySelectorAll('table').length).toBe(0);
+  });
 });
